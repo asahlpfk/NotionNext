5a2839d0c29259d765094e3316d52e5f86b61a01
asahlpfk <asahlpfk@gmail.com>
2024-12-07 17:01:59 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2024-01-06', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
@@ -19,7 +19,7 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '随机分享',
   HEO_HERO_TITLE_2: '图片视频',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_3: '',
   HEO_HERO_TITLE_4: '欢迎来到',
   HEO_HERO_TITLE_5: '我的博客',
   HEO_HERO_TITLE_LINK: 'https://',
